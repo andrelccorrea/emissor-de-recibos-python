ff8401cb4ff5fa970868e21230a68e53771fa925
Andre Correa <andre.lcosta@hotmail.com>
2020-04-14 22:22:50 -0300

    criada interface de teste com Kivy

--- gui_recibos.py
+++ gui_recibos.py
@@ -1,43 +1,39 @@
-import kivy
-kivy.require('1.11.1')
-
-# a classe principal do aplicativo deve estender a classe App
 from kivy.app import App
-# widgets usados para trabalhar com múltiplas telas
+from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-# widgets usados para alterar as cores de fundo dos componentes e telas
-from kivy.graphics import Color, Rectangle
-# demais widgets que serão usados no projeto
-from kivy.uix.label import Label
 
+# carrega as definições da interface gráfica
+# usei Builder.load_string com o método open, passando a codificação
+# em utf-8, pois se usar o método load_file, passando direto o arquivo
+# gui.kv, os acentos não são reconhecidos
+Builder.load_string(open("gui.kv",encoding="utf-8").read())
+
+# telas
 class TelaInicial(Screen):
+    pass
 
-    # necessário sobrescrever o método init da classe Screen, passando os argumentos
-    # para que os widgets possam ser adicionados e configurados na inicialização
-    def __init__(self,**kwargs):
-        super(TelaInicial, self).__init__(**kwargs)
-        # widgets
-        self.add_widget(Label(text='Teste telas', color=[1,0,0,1]))
+class TelaCadastroRecebedor(Screen):
+    pass
 
-        # define a cor da tela
-        with self.canvas.before:
-            Color(0,1,0,1)
-            self.rect = Rectangle(size=self.size,
-                                  pos=self.pos)
-        # vincula o retângulo colorido para que seja redimensionado juntamente com a tela do app
-        self.bind(size=self._update_rect, pos=self._update_rect)
-    
-    # atualiza o tamanho e posição do retângulo
-    def _update_rect(self, instance, value):
-        self.rect.pos = instance.pos
-        self.rect.size = instance.size
+class TelaCadastroPagador(Screen):
+    pass
 
-class MyApp(App):
+class TelaConsultaPagadores(Screen):
+    pass
+
+# cria o gerenciador de telas e adiciona as telas
+# atenção aos nomes passados como parâmetro, pois
+# são usados no arquivo gui.kv para navegação
+sm = ScreenManager()
+sm.add_widget(TelaInicial(name='tela_inicial'))
+sm.add_widget(TelaCadastroRecebedor(name='tela_cadastro_recebedor'))
+sm.add_widget(TelaCadastroPagador(name='tela_cadastro_pagador'))
+sm.add_widget(TelaConsultaPagadores(name='tela_consultar_pagadores'))
+
+class Main(App):
 
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(TelaInicial(name='tela_inicial'))
         return sm
 
 if __name__ == '__main__':
-    MyApp().run()
+    Main().run()
