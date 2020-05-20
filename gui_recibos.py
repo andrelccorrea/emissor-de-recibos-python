731a7d125612fa14345f96d841b6b2b63996caa7
Juliano <julianocf.2009@gmail.com>
2020-05-20 08:47:42 -0300

    cadastro e listagem de itens

--- gui_recibos.py
+++ gui_recibos.py
@@ -22,13 +22,19 @@ from kivy.core.text import LabelBase
 # carrega as fontes (fonte free para uso comercial / baixei de fontsquirrel.com)
 LabelBase.register(name = "AlexBrush", fn_regular = "fonts/AlexBrush-Regular.ttf")
 # usa StringProperty para carregar dados dinamicamente
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ListProperty
 # banco de dados
 import sqlite3
 import banco_sqlite as banco
 # mensagens em popup
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
+# listagem
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.button import Button
+from kivy.uix.recyclegridlayout import RecycleGridLayout
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 
 # telas
 class TelaInicial(Screen):
@@ -47,15 +53,15 @@ class TelaConsultarPagadores(Screen):
     pass
 
 class TelaCadastrarItens(Screen):
-    def exibir_popup_do_erro(self):
-        pop = Popup(title='Campos inválidos',
-                    content=Label(text='Preencha corretamente todos os campos.'),
+    def exibir_popup(self, titulo, mensagem):
+        pop = Popup(title=titulo,
+                    content=Label(text=mensagem),
                     size_hint=(None, None), size=(350, 120))
         pop.open()
 
     def validar_campos(self):
-        if not self.ids.descricao.text or not self.ids.preco.text or float(self.ids.preco.text) <= 0:
-            self.exibir_popup_do_erro()        
+        if not self.ids.txt_descricao.text or not self.ids.txt_preco.text or float(self.ids.txt_preco.text) <= 0:
+            self.exibir_popup('Campos inválidos','Preencha corretamente todos os campos.')        
             return False
         return True
 
@@ -63,18 +69,44 @@ class TelaCadastrarItens(Screen):
         # abre uma conexão com o banco
         conexao = sqlite3.connect("recibos.db")
         # junta os dados do form
-        dados = [self.ids.descricao.text, self.ids.preco.text]
+        dados = [self.ids.txt_descricao.text, self.ids.txt_preco.text]
         # grava os dados
         banco.cadastrar_item(conexao, dados)
         # limpa os campos
-        self.ids.descricao.text = ""
-        self.ids.preco.text = ""
+        self.ids.txt_descricao.text = ""
+        self.ids.txt_preco.text = ""
         # fecha a conexão com o banco
         conexao.close()
+        self.exibir_popup('Cadastro','Item cadastrado com sucesso!')
         return
 
 class TelaConsultarItens(Screen):
-    pass    
+    data_items = ListProperty([])
+
+    def __init__(self, **kwargs):
+        super(TelaConsultarItens, self).__init__(**kwargs)
+        self.get_users()
+
+    def get_users(self):
+        connection = sqlite3.connect("recibos.db")
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT descricao,valor FROM itens ORDER BY item_id ASC")
+        rows = cursor.fetchall()
+
+        # create data_items
+        for row in rows:
+            for col in row:
+                self.data_items.append(col)   
+
+class SelectableRecycleGridLayout(FocusBehavior, LayoutSelectionBehavior, RecycleGridLayout):
+    pass
+
+class TableHeaderLabel(RecycleDataViewBehavior, Label):
+    pass
+
+class TableRowLabel(RecycleDataViewBehavior, Label):
+    pass
 
 class TelaEmitirRecibo(Screen):
     pass
